f3465297373282b0060db075f1d8e673534c70f4
DANIEL JOHNSTON <danjohnston93@cox.net>
2018-03-06 02:00:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,12 +11,23 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
+ var itemPrice = Math.floor(Math.random() * 100);
+ cart.push(new Object({[item]:itemPrice}));
+ console.log(`${item} has been added to your cart.`);
+ return cart;
 }
 
 function viewCart() {
   // write your code here
+  if (cart.length === 0) {
+    return `Your shopping cart is empty.`;
+  }
+  if (cart.length === 1) {
+    return `In your cart, you have ${
+  }
 }
-
+ 
+ 
 function total() {
   // write your code here
 }
